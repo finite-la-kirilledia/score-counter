e3b54044fb070caedf04d21eb20856a754c8e5c1
Kirill Minko <kirill040994@gmail.com>
2020-05-19 23:26:05 +0300

    add ScoreViewModel to data binding

--- app/src/main/java/com/example/scorecounter/MainActivity.kt
+++ app/src/main/java/com/example/scorecounter/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.scorecounter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.example.scorecounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var scoreViewModel: ScoreViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        scoreViewModel = ViewModelProviders.of(this).get(ScoreViewModel::class.java)
+        binding.scoreViewModel = scoreViewModel
     }
 }
